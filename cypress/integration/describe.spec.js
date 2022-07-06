2900702755c8e2172bd48e03cfc3d3be5bbc33b2
Felipe Almeida <felipe.almeida@i-value.com.br>
2022-07-06 00:15:57 -0300

    Curso Cypress - Aplicacoes Modernas

---
+++ cypress/integration/describe.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+it('A external test', () =>{
+
+
+})
+
+describe.skip('Should group testes', () => {
+  
+  describe('Should group more specific testes', () => {
+    it('A specific teste', () =>{
+    })
+  })
+
+  it('A internal test', () =>{
+
+
+  })
+  
+})
